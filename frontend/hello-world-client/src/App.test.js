649d14ba89bb9085e6123c27f3c0e6fa4f5e7af9
Alfonso De La Rosa <alfdelarosa4@gmail.com>
2024-02-14 15:04:31 -0800

    modify test

--- frontend/hello-world-client/src/App.test.js
+++ frontend/hello-world-client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the label with "Enter your name:"', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const labelElement = screen.getByText('Enter your name:');
+  expect(labelElement).toBeInTheDocument();
 });
